b6ffd0253649f3e94653404d9d43fbc5a96ed7f2
Andy Maloney <asmaloney@gmail.com>
2016-07-14 21:15:59 -0400

    Switches out MapQuest tiles for OSM
    MapQuest discontinued their public tiles and now require an API key, so switch them out for the OpenStreetMap ones

--- maps/leaf-demo.js
+++ maps/leaf-demo.js
@@ -6,9 +6,9 @@ var map = L.map( 'map', {
   zoom: 2
 });
 
-L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-  subdomains: ['otile1','otile2','otile3','otile4']
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+ subdomains: ['a','b','c']
 }).addTo( map );
 
 var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
