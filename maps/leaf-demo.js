7c04cde9cb81a5bfd5e54471c5d625e38b48a278
Andy Maloney <asmaloney@gmail.com>
2021-05-06 18:14:05 -0400

    Update with current versions and practices.
    - Updated to Leaflet 1.7.1, leaflet.markercluster 1.5.0, and jQuery 3.6.0.
    - Added Subresource Integrity to CSS and CDN JavaScript.
    - All files reformatted with "prettier" formatter.
    - Use https for URLs.

--- maps/leaf-demo.js
+++ maps/leaf-demo.js
@@ -1,41 +1,51 @@
 // See post: http://asmaloney.com/2015/06/code/clustering-markers-on-leaflet-maps
 
-var map = L.map( 'map', {
+var map = L.map('map', {
   center: [10.0, 5.0],
   minZoom: 2,
-  zoom: 2
-});
+  zoom: 2,
+})
 
-L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
- attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
- subdomains: ['a','b','c']
-}).addTo( map );
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: ['a', 'b', 'c'],
+}).addTo(map)
 
-var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
+var myURL = jQuery('script[src$="leaf-demo.js"]')
+  .attr('src')
+  .replace('leaf-demo.js', '')
 
 var myIcon = L.icon({
   iconUrl: myURL + 'images/pin24.png',
   iconRetinaUrl: myURL + 'images/pin48.png',
   iconSize: [29, 24],
   iconAnchor: [9, 21],
-  popupAnchor: [0, -14]
-});
+  popupAnchor: [0, -14],
+})
 
-var markerClusters = L.markerClusterGroup();
+var markerClusters = L.markerClusterGroup()
 
-for ( var i = 0; i < markers.length; ++i )
-{
-  var popup = markers[i].name +
-              '<br/>' + markers[i].city +
-              '<br/><b>IATA/FAA:</b> ' + markers[i].iata_faa +
-              '<br/><b>ICAO:</b> ' + markers[i].icao +
-              '<br/><b>Altitude:</b> ' + Math.round( markers[i].alt * 0.3048 ) + ' m' +
-              '<br/><b>Timezone:</b> ' + markers[i].tz;
+for (var i = 0; i < markers.length; ++i) {
+  var popup =
+    markers[i].name +
+    '<br/>' +
+    markers[i].city +
+    '<br/><b>IATA/FAA:</b> ' +
+    markers[i].iata_faa +
+    '<br/><b>ICAO:</b> ' +
+    markers[i].icao +
+    '<br/><b>Altitude:</b> ' +
+    Math.round(markers[i].alt * 0.3048) +
+    ' m' +
+    '<br/><b>Timezone:</b> ' +
+    markers[i].tz
 
-  var m = L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
-                  .bindPopup( popup );
+  var m = L.marker([markers[i].lat, markers[i].lng], {
+    icon: myIcon,
+  }).bindPopup(popup)
 
-  markerClusters.addLayer( m );
+  markerClusters.addLayer(m)
 }
 
-map.addLayer( markerClusters );
+map.addLayer(markerClusters)
